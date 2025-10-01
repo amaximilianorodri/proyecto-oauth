a1b0caf15cef7d39d2b75fa9c8b2dc8227e39a0d
Amparito Maximiliano <u22301756@utp.edu.pe>
2025-10-01 16:45:07 -0500

    Amparito agregó autenticación OAuth2

--- auth-service.js
+++ auth-service.js
@@ -1,12 +1,9 @@
 // auth-service.js
 
-function iniciarSesionConOAuth2(token) {
-  if (!token) {
-    throw new Error("Token inválido o ausente");
-  }
-  console.log("Autenticación exitosa con OAuth2 🎉");
-  return true;
+function loginUser(email, password) {
+  console.log("Autenticación con OAuth2 activada");
+  // Código nuevo de Amparito
 }
 
+
 iniciarSesionConOAuth2("token_de_prueba");
-"// auth-service.js" 
